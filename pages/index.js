df9495dd5062dee6e57f452847df0917f1391175
unknown <saifuliqbalh@gmail.com>
2025-04-30 09:33:44 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Button, FlexBox, Input } from "../components/UIComponents"
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className=" w-[85%] flex flex-row gap-5 max-w-800">
+      <div className=" w-[85%] flex flex-col gap-5 max-w-800">
           <Input placeholder="Masukkan username"/>
           <Input placeholder="Masukkan sandi" type="password"/>
           <Button text="Login"/>
