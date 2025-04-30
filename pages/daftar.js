91ad5be86a11f73e00feb25694a4bba5002d7efe
unknown <saifuliqbalh@gmail.com>
2025-04-30 10:50:27 +0700

    add

--- pages/daftar.js
+++ pages/daftar.js
@@ -3,7 +3,7 @@ import { Button, Input } from "../components/UIComponents"
 export default function Daftar() {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="w=[85%] flex flex-col gap-6 w-100">
+            <div className="w-[85%] flex flex-col gap-6 w-100">
             <h1 className="text-4xl text-center font-bold">Daftar disini</h1>
                 <Input placeholder="Nama kamu"/>
                 <Input placeholder="Buat password"/>
