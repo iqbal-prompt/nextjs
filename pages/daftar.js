0053d424e5e642c5ebe82cb6602f720fad777bfa
unknown <saifuliqbalh@gmail.com>
2025-04-30 11:32:22 +0700

    add

--- pages/daftar.js
+++ pages/daftar.js
@@ -1,14 +1,18 @@
 import { Button, Input } from "../components/UIComponents"
-
+import Link from "next/link"
 export default function Daftar() {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="w-[85%] flex flex-col gap-6 max-w-100">
+            <div className="w-[85%] flex flex-col gap-6 max-w-[330px]">
             <h1 className="text-4xl text-center font-bold">Daftar disini</h1>
                 <Input placeholder="Nama kamu"/>
                 <Input placeholder="Buat password"/>
                 <Button text="Daftar"/>
-                <span className="text-center font-bold"><a href="/">Login disini</a></span>
+                <div className="text-center">
+                    <Link href="/">
+                        <span className="font-bold text-blue-400 hover:underline">Login disini</span>
+                    </Link>
+                </div>
             </div>
         </div> 
     );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import { Button, FlexBox, Input } from "../components/UIComponents"
+import Link from "next/link"
+
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className=" w-[85%] flex flex-col gap-6 max-w-100">
+      <div className=" w-[85%] flex flex-col gap-6 max-w-[330px]">
       <h1 className="text-center font-bold text-4xl">Login disini</h1>
           <Input placeholder="Masukkan username"/>
           <Input placeholder="Masukkan sandi" type="password"/>
           <Button text="Login"/>
-          <span className="text-center font-bold"><a href="/daftar">Daftar disini</a></span>
+          <div className="text-center">
+            <Link href="/daftar">
+              <span className="font-bold text-blue-400 hover:underline">Daftar disini</span>
+            </Link>
+          </div>
       </div>
     </div>
   );
