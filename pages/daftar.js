21137c25c41e17f71481807f097419471ed9bf83
unknown <saifuliqbalh@gmail.com>
2025-04-30 11:53:03 +0700

    update

--- pages/daftar.js
+++ pages/daftar.js
@@ -1,10 +1,11 @@
 import { Button, Input } from "../components/UIComponents"
 import Link from "next/link"
+
 export default function Daftar() {
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="w-[85%] flex flex-col gap-6 max-w-[330px]">
-            <h1 className="text-4xl text-center font-bold">Daftar disini</h1>
+            <h1 className="text-3xl text-center font-bold">Daftar disini</h1>
                 <Input placeholder="Nama kamu"/>
                 <Input placeholder="Buat password"/>
                 <Button text="Daftar"/>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import { Button, FlexBox, Input } from "../components/UIComponents"
+import { Button, Input } from "../components/UIComponents"
 import Link from "next/link"
 
 export default function Page() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className=" w-[85%] flex flex-col gap-6 max-w-[330px]">
-      <h1 className="text-center font-bold text-4xl">Login disini</h1>
-          <Input placeholder="Masukkan username"/>
-          <Input placeholder="Masukkan sandi" type="password"/>
-          <Button text="Login"/>
-          <div className="text-center">
-            <Link href="/daftar">
-              <span className="font-bold text-blue-400 hover:underline">Daftar disini</span>
-            </Link>
-          </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="w-[85%] flex flex-col gap-6 max-w-[330px]">
+            <h1 className="text-center font-bold text-3xl">Login disini</h1>
+                <Input placeholder="Masukkan username"/>
+                <Input placeholder="Masukkan sandi" type="password"/>
+                <Button text="Login"/>
+                <div className="text-center">
+                    <Link href="/daftar">
+                        <span className="font-bold text-blue-400 hover:underline">Daftar disini</span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
