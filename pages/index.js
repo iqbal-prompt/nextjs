e3809ff4898770ec70d78a5879299c773978bcdb
unknown <saifuliqbalh@gmail.com>
2025-05-02 22:05:11 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import { useState } from "react"
 import { Button, Input } from "../components/UIComponents"
 import Link from "next/link"
 
 export default function Page() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        const res = await fetch("https://vjb0sm07-4000.asse.devtunnels.ms/api/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password })
+        })
+        const data = await res.json();
+        alert(data.message)
+    }
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="w-[85%] flex flex-col gap-6 max-w-[330px]">
             <h1 className="text-center font-bold text-3xl">Login disini</h1>
-                <Input placeholder="Masukkan username"/>
-                <Input placeholder="Masukkan sandi" type="password"/>
-                <Button text="Login"/>
+                <Input placeholder="Masukkan username" onChange={(e) => setUsername(e.target.value)} />
+                <Input placeholder="Masukkan sandi" type="password" onChange={(e) => setPassword(e.target.value)} />
+                <Button text="Login" onClick={handleLogin} />
                 <div className="text-center">
                     <Link href="/daftar">
                         <span className="font-bold text-blue-400 hover:underline">Daftar disini</span>
