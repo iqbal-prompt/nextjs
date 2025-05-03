475a2c53284f737563c506bbecd18a7489167a90
unknown <saifuliqbalh@gmail.com>
2025-05-03 11:19:05 +0700

    update

--- components/UIComponents.js
+++ components/UIComponents.js
@@ -1,6 +1,7 @@
-export function Button({text, bgColor = 'bg-blue-500', hoverColor = 'hover:bg-blue-600'}) {
+export function Button({text, onClick, bgColor = 'bg-blue-500', hoverColor = 'hover:bg-blue-600'}) {
     return (
         <button 
+        onClick={onClick}
         className={`px-6 py-3 ${bgColor} text-white font-bold tracking-wide rounded-lg cursor-pointer ${hoverColor} transition-color duration-300`}>
         {text}
         </button>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,31 @@
+"use client"
+
 import { useState } from "react"
 import { Button, Input } from "../components/UIComponents"
 import Link from "next/link"
 
 export default function Page() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleLogin = async () => {
-        console.log("Ngentodddd.......")
+        console.log("Allohuakbarr")
         const res = await fetch("https://vjb0sm07-4000.asse.devtunnels.ms/api/login", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({ username, password })
         })
-        const data = await res.json();
+
+        const data = await res.json()
         alert(data.message)
     }
+
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="w-[85%] flex flex-col gap-6 max-w-[330px]">
-            <h1 className="text-center font-bold text-3xl">Login disini</h1>
+                <h1 className="text-center font-bold text-3xl">Login disini</h1>
                 <Input placeholder="Masukkan username" onChange={(e) => setUsername(e.target.value)} />
                 <Input placeholder="Masukkan sandi" type="password" onChange={(e) => setPassword(e.target.value)} />
                 <Button text="Login" onClick={handleLogin} />
